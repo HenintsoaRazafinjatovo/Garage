35a12b8347a12428e250a20963f382ae8254005e
Labarre12 <labarreitokiana@gmail.com>
2024-07-15 18:41:30 +0300

    Update login

--- application/views/Login.php
+++ application/views/Login.php
@@ -103,7 +103,7 @@
   <script src="<?php echo base_url("assets/vendor/apexcharts/apexcharts.min.js")  ?>"></script>
   <script src="<?php echo base_url("assets/vendor/bootstrap/js/bootstrap.bundle.min.js")  ?>"></script>
   <script src="<?php echo base_url("assets/vendor/chart.js/chart.min.js")  ?>"></script>
-  <script src="<?php echo base_url(" assets/vendor/echarts/echarts.min.js") ?>"></script>
+  <script src="<?php echo base_url("assets/vendor/echarts/echarts.min.js") ?>"></script>
   <script src="<?php echo base_url("assets/vendor/quill/quill.min.js")  ?>"></script>
   <script src="<?php echo base_url("assets/vendor/simple-datatables/simple-datatables.js") ?>"></script>
   <script src="<?php echo base_url("assets/vendor/tinymce/tinymce.min.js")  ?>"></script>
