89f6bc8e9a8be1ff00946a6088ea196ba2dd5348
Labarre12 <labarreitokiana@gmail.com>
2024-07-15 16:27:45 +0300

    Rdv
    avec header, sidebar , footer

---
+++ application/views/Rdv.php
@@ -0,0 +1,40 @@
+    
+        <main id="main" class="main">
+            <div class="pagetitle">
+                <h1>Prendre rendez-vous</h1>
+            </div>
+            <div>
+                <form>
+                    <div class="row mb-3">
+                        <label for="inputDate" class="col-sm-2 col-form-label">Date</label>
+                        <div class="col-sm-10">
+                            <input type="date" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <label for="inputTime" class="col-sm-2 col-form-label">Time</label>
+                        <div class="col-sm-10">
+                            <input type="time" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                    <label class="col-sm-2 col-form-label">Type de service</label>
+                    <div class="col-sm-10">
+                        <select class="form-select" aria-label="Default select example">
+                        <option selected>Choisissez votre type de service</option>
+                        <option value="1">Simple</option>
+                        <option value="2">Standard</option>
+                        <option value="3">Complexe</option>
+                        </select>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">Valider</button>
+                        </div>
+                    </div>
+
+                </form>
+        </main>
+
+        
