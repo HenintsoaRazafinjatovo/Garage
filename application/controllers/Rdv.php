a95f55a4538f09bc26474ff10a811ae7ecb7a8e7
Mirado <kainrak312@gmail.com>
2024-07-15 17:46:37 +0300

    RdvController

--- application/controllers/Rdv.php
+++ application/controllers/Rdv.php
@@ -7,6 +7,23 @@ class Rdv extends CI_Controller {
 	{
 		$data['content']='Rdv';
 		$data['user']=$this->session->userdata("client");
+		$this->load->model('ServiceModel');
+		$data['services']=$this->ServiceModel->getAll();
 		$this->load->view('partials/template',$data);
+	}
+
+	public function demandeRdv(){
+		$dateDebut=$this->input->post("date");
+		$heureDebut=$this->input->post("heure");
+
+		$dateHDebut=new DateTime($dateDebut+" "+$heureDebut);
+		$idService=$this->input->post("type");
+		$idClient=$this->session->userdata("client")['Id_Client'];
+
+		$this->load->model('RdvModel');
+		try{
+			$test=$this->RdvModel->demandeRdv($dateHDebut,$idService,$idClient);
+
+		}
 	}
 }
