a45e14eeb779090e029f3887137022f4cd78d811
Labarre12 <labarreitokiana@gmail.com>
2024-07-16 17:39:40 +0300

    Update sidebar.php

--- application/views/partials/sidebar.php
+++ application/views/partials/sidebar.php
@@ -4,43 +4,43 @@
             <ul class="sidebar-nav" id="sidebar-nav">
 
                 <li class="nav-item">
-                    <a class="nav-link " href="#">
+                    <a class="nav-link " href="<?php echo site_url('rdv') ?>">
                     <i class="bi bi-grid"></i>
                     <span>Rendez-vous</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " href="#">
+                    <a class="nav-link " href="<?php echo site_url('services') ?>">
                     <i class="bi bi-grid"></i>
                     <span>Services</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " href="#">
+                    <a class="nav-link " href="<?php echo site_url('devis') ?>">
                     <i class="bi bi-grid"></i>
                     <span>Liste devis</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " href="#">
+                    <a class="nav-link " href="">
                     <i class="bi bi-grid"></i>
                     <span>Import</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " href="#">
+                    <a class="nav-link " href="<?php echo site_url('') ?>">
                     <i class="bi bi-grid"></i>
                     <span>Dashboard</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " href="#">
+                    <a class="nav-link " href="<?php echo site_url('slot') ?>">
                     <i class="bi bi-grid"></i>
                     <span>Liste slot</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " href="#">
+                    <a class="nav-link " href="<?php echo site_url('devis/deleteDatas') ?>">
                     <i class="bi bi-grid"></i>
                     <span>Supprimer donnees</span>
                     </a>
